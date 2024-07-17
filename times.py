1e4da7fe0f1e27c658c381d5408945058c87b8bc
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-07-17 06:16:38 -0400

    added time tracker

--- times.py
+++ times.py
@@ -1,9 +1,14 @@
 from datetime import datetime
-from config import TIMES
-from config import WENDTIMES
 # strptime all of it
 
-def calculateTimes(cDay, isWednesday, hasEarlyLunch = False):
-    times = ["8:15","9:10","10:05","10:35","11:50","12:25" if ealunch else "12:40", "1:20","2:15", "3:05", "3:35"]
-    if isWednesday:
-        times = ["9:35", "10:30","11:25", "12:20", "12:45" if ealunch else "1:10", "1:40", "2:35", "3:30"]
+class period:
+    def __init__(cDay, isWednesday, ealunch = false):
+        calculateTimes()
+            
+    def calculateTimes(self):
+        ties = ["8:15","9:10","10:05","10:35","11:50","12:25" if self.ealunch else "12:40", "13:20","14:15", "15:05", "15:35"]
+        if self.isWednesday:
+            ties = ["9:35", "10:30","11:25", "12:20", "12:45" if self.ealunch else "13:10", "13:40", "14:35", "15:30"]
+
+        return [datetime.strptime(x,"%H:%M").time() for x in ties]
+
