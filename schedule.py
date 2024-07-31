8affb884d525e58bdd2096915cfe97847db06189
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-07-30 21:56:09 -0400

    STUFF!

---
+++ schedule.py
@@ -0,0 +1,14 @@
+import datetime as dt
+
+
+class schedule:
+    def __init__(self):
+        pass
+
+examplePeriod = {
+        "periodNum":3,
+        "time":dt.time(8,5), #8:05 AM
+        "periodName":"Feeding",
+        "periodSymbol":"🌦",
+} 
+
--- times.py
+++ times.py
@@ -2,13 +2,20 @@ from datetime import datetime
 # strptime all of it
 
 class period:
-    def __init__(cDay, isWednesday, ealunch = false):
-        calculateTimes()
+    def __init__(self, cDay, isWednesday, ealunch = False):
+        self.ealunch = ealunch
+        self.cDay = cDay
+        self.isWednesday = isWednesday
+        self.calculateTimes() # Sets self.times
             
     def calculateTimes(self):
         ties = ["8:15","9:10","10:05","10:35","11:50","12:25" if self.ealunch else "12:40", "13:20","14:15", "15:05", "15:35"]
         if self.isWednesday:
             ties = ["9:35", "10:30","11:25", "12:20", "12:45" if self.ealunch else "13:10", "13:40", "14:35", "15:30"]
 
-        return [datetime.strptime(x,"%H:%M").time() for x in ties]
+        self.times =  [datetime.strptime(x,"%H:%M").time() for x in ties]
+    
 
+if __name__ == "__main__":
+    perd = period(1j, False)
+    print(perd.times)
