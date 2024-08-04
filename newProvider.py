45795db1f979d62c060d0f0b913296eeb5526edf
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2024-08-04 10:43:13 -0400

    Started newProvider that uses new engine for calculation

---
+++ newProvider.py
@@ -0,0 +1,102 @@
+# System
+import argparse
+import datetime as dt
+import time
+
+# User
+import calRead
+import schedule
+
+parser = argparse.ArgumentParser(
+                    prog='dailyInfo provider',
+                    description='Provides daily information for school schedule') #,epilog='txt at end of help')
+
+parser.add_argument('--dateoffset', type=int)
+parser.add_argument('--nextdaytime', type=str, help="not implemented")
+parser.add_argument('-p', '--prompt',
+                    action='store_true')
+
+args = parser.parse_args()
+
+offset = args.dateoffset
+if offset is None:
+    offset = 0
+
+if args.prompt:
+    try:
+        offset = int(input("offset > "))
+    except:
+        print("-> 0")
+
+cycleDay = calRead.cycleDay(offset, verbose=True)
+
+# Check day of week
+isWeekday = True
+isWednesday = False    
+match dt.datetime.now().weekday():
+    case 2:
+        isWednesday = True
+    case 5:
+        isWeekday = False    
+    case 6:
+        isWeekday = False
+
+
+dayISchedulePeriodList = [
+        {
+            "eaLunch": True,
+            "periodName":"Composing",
+            "periodSymbol":"🎻",
+        },
+        {
+            "eaLunch": True,
+            "periodName":"Computer Scinece",
+            "periodSymbol":"💻",
+        },
+        {
+            "eaLunch": True,
+            "periodName":"Wayfinding",
+            "periodSymbol":"🧭",
+        },
+        {
+            "eaLunch": False,
+            "periodName":"Division 201",
+            "periodSymbol":"➗",
+        },
+        {
+            "eaLunch": False,
+            "periodName":"Cryptography II",
+            "periodSymbol":"📜",
+        },
+        {
+            "eaLunch": True,
+            "periodName":"Potion-making",
+            "periodSymbol":"🧪",
+        },
+        {
+            "eaLunch": False,
+            "periodName":"Magic Charms 101",
+            "periodSymbol":"🪄",
+        },
+        {
+            "periodName":"Free",
+            "periodSymbol":"🆓",
+        },
+]
+
+
+pl = schedule.generatePeriodList(dayISchedulePeriodList, cycleDay, isWednesday)
+for i in pl:
+    print(i)
+for i in pl:
+    print(i["datetime"], i["duration"])
+
+sch = schedule.schedule(pl)
+
+print("eaLunch:",sch.getEaLunch())
+
+for i in range(-100,120):
+    sch.updatePeriod(testDeltaTime=dt.timedelta(minutes = i))
+    timeLeft = sch.getPeriodTimeLeft()
+    print(sch.getPeriodName(), sch.getPeriodSymbol(), timeLeft, timeLeft >= dt.timedelta(0), "          ", end="\r")
+    time.sleep(0.2)
